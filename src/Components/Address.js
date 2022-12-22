011704fe2545d07a288f2858200abf331f293603
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2022-12-22 16:35:20 +0100

    Add files via upload

---
+++ src/Components/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+
+        <h3 className='home'> Cité Dhamen - Béja Nord - Béja</h3>
+
+  )
+}
+
+export default Address
---
+++ src/Components/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img className='redcard' src="/Aboubaker_Ismail_2022.jpg" alt="ref" />
+        
+    </div>
+  )
+}
+
+export default ProfilePhoto
